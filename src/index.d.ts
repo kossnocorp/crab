6ed7d1bfce04671b257a7668eb80e4417d005f4b
Sasha Koss <koss@nocorp.me>
2024-07-22 15:44:31 +0800

    Add CNProps

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,6 @@ import { type Crab } from "./types.js";
 export type { Crab };
 
 export const cn: Crab.Factory;
+
+export type CNProps<Renderer extends Crab.Renderer<Crab.VariantsConstrain>> =
+  Renderer extends Crab.Renderer<infer Variants> ? Partial<Variants> : never;
